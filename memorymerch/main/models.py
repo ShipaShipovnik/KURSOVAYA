2f507de6c372cf7ef9a513ef78d41458c4409496
admin <admin@44-10>
2024-03-13 13:10:33 +0300

    Починила форму объявления(товара)

--- memorymerch/main/models.py
+++ memorymerch/main/models.py
@@ -13,11 +13,11 @@ class Categories(models.Model):
 class Tovar(models.Model):
     tovarname = models.CharField(max_length=100, blank=False, default='')
     tovarprice = models.FloatField()
-    category = models.ForeignKey('Categories', related_name='tovars_categories', on_delete=models.CASCADE)
+    category = models.ForeignKey('Categories', related_name='tovars_categories', on_delete=models.CASCADE,null=True)
     shipping = models.TextField(max_length=300, blank=False, default='')
     tovardescrpt = models.TextField(max_length=500, blank=False, default='')
-    date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE,default='')
+    date_posted = models.DateTimeField(default=timezone.now,null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,default='',null=True)
 
     def __str__(self):
         return f'{self.tovarname} {self.author} {self.category}'
