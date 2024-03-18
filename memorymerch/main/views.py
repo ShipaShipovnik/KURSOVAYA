b2de03ff9bcb93209623d875245c3301afb36027
admin <admin@44-10>
2024-03-18 12:40:23 +0300

    Фильтрация

--- memorymerch/main/views.py
+++ memorymerch/main/views.py
@@ -1,35 +1,37 @@
-from django.shortcuts import render,redirect
-from .models import Post,Tovar
+from django.shortcuts import render, redirect
+from .models import Post, Tovar
 from .forms import TovarForm
+from .filters import TovarFilter
 
 
 def index(request):
-    context = {
-        'tovars': Tovar.objects.all(),
-        'posts': Post.objects.all()
-    }
-    return render(request, 'main/index.html',context)
+    tovars_all=Tovar.objects.all(), 
+    posts = Post.objects.all()
+    filter_tovar= TovarFilter(request.GET, queryset=Tovar.objects.all())
+    return render(request, "main/index.html",{'tovars':filter_tovar,'posts':posts})
+
 
 def about(request):
-    return render(request, 'main/about.html')
+    return render(request, "main/about.html")
+
 
 def rules(request):
-    return render(request, 'main/rules.html')
+    return render(request, "main/rules.html")
+
 
 def createtovar(request):
-    error = ''
-    if request.method == 'POST':
+    error = ""
+    if request.method == "POST":
         form = TovarForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('index')
+            return redirect("index")
         else:
-            error = 'Форма заполнена неправильно или не до конца'
+            error = "Форма заполнена неправильно или не до конца"
     form = TovarForm()
-    context = {
-        'form': form
-    }
-    return render(request, 'main/create-tovar.html', context)
+    context = {"form": form}
+    return render(request, "main/create-tovar.html", context)
+
 
 def tovar(request):
-     return render(request, 'main/tovar.html')
+    return render(request, "main/tovar.html")
